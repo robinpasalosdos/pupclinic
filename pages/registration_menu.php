1c363041a5bf98a6301410e215698fae6dbed9a9
Robin <robinpasalosdos@gmail.com>
2024-01-24 04:15:08 +0800

    Registration Menu Updated

--- pages/registration_menu.php
+++ pages/registration_menu.php
@@ -1,3 +1,13 @@
-<label>register as:</label><br><br>
-<a href="../pupclinic/index.php?page=registration&user=student">Student</a><br>
-<a href="../pupclinic/index.php?page=registration&user=faculty">Faculty</a>
+<div class="menu_container">
+    <button onclick="redirectToStudentRegistration()">Register as Student</button> 
+    <button onclick="redirectToFacultyRegistration()">Register as Faculty</button>
+</div>
+
+<script>
+  function redirectToStudentRegistration() {
+    window.location.href = "../pupclinic/index.php?page=registration&user=student";
+  }
+  function redirectToFacultyRegistration() {
+    window.location.href = "../pupclinic/index.php?page=registration&user=faculty";
+  }
+</script>
