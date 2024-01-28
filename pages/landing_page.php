5ad57423b284b8c9b71758d33bbff00d7d6de320
Equirizon <equirizon@gmail.com>
2024-01-28 18:08:09 +0800

    `Landing Page` styling

--- pages/landing_page.php
+++ pages/landing_page.php
@@ -1,15 +1,7 @@
 <div class="main_container">
-    <div class="container">
-        <div class="index_lp_1">
-            <img alt="pup-logo" src="../pupclinic/assets/pup-logo.png">
-        </div>
-        <div class="index_lp_2">
-            <h1>PUP CLINIC</h1>
-        </div>
-    </div>
-    <div class="button_container">
-        <button onclick="redirectToLocation()">Start</button>
-    </div>
+    <img alt="pup-logo" src="../pupclinic/assets/pup-logo.png">
+    <h1>PUP CLINIC</h1>
+    <button onclick="redirectToLocation()">Start</button>
 </div>
     
 
