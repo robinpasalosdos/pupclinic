620b54d855f60246fc2fb0ca6081fa479533b18a
Equirizon <equirizon@gmail.com>
2024-01-29 01:36:13 +0800

    added `register` styling

--- pages/registration.php
+++ pages/registration.php
@@ -1,41 +1,44 @@
 <?php if($_GET['user'] == 'student'): ?>
-    <form id="register">
-        <label for="student_no">Student Number</label><br>
-        <input type="text" id="student_no" name="student_no" required autocomplete="off"><br>
-        <label for="name">Name</label><br>
-        <input type="text" id="name" name="name" required autocomplete="off"><br>
-        <label for="course">Course</label><br>
-        <input type="text" id="course" name="course" required autocomplete="off"><br>
-        <label for="year">Year</label>
-        <select name="year" id="year" require>
-                
+    <div class="login-form">
+        <h1>Register as Student</h1>
+        <form id="register">
+            <label for="student_no">Student Number</label>
+            <input type="text" id="student_no" name="student_no" required autocomplete="off"><br>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" required autocomplete="off"><br>
+            <label for="course">Course</label>
+            <input type="text" id="course" name="course" required autocomplete="off"><br>
+            <label for="year">Year</label>
+            <select name="year" id="year" require>   
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
                 <option value="5">5</option>
-    
-        </select><br>
-        
-        <label for="section">Section</label><br>
-        <input type="text" id="section" name="section" required autocomplete="off"><br>
-        <label for="email">Email</label><br>
-        <input type="text" id="email" name="email" required autocomplete="off"><br>
-        <label for="password">Password</label><br>
-        <input type="password" id="password" name="password" required autocomplete="off"><br>
-        <button>Save</button>
-    </form>
+            </select><br>
+            <label for="section">Section</label>
+            <input type="text" id="section" name="section" required autocomplete="off"><br>
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" required autocomplete="off"><br>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required autocomplete="off"><br>
+            <button>Save</button>
+        </form>
+    </div>
 <?php endif; ?>
 <?php if($_GET['user'] == 'faculty'): ?>
-    <form id="register">   
-        <label for="name">Name</label><br>
-        <input type="text" id="name" name="name" required autocomplete="off"><br>
-        <label for="email">Email</label><br>
-        <input type="text" id="email" name="email" required autocomplete="off"><br>
-        <label for="password">Password</label><br>
-        <input type="password" id="password" name="password" required autocomplete="off"><br>
-        <button>Save</button>
-    </form>
+    <div class="login-form">
+        <h1>Register as Faculty</h1>
+        <form id="register">   
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" required autocomplete="off"><br>
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" required autocomplete="off"><br>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required autocomplete="off"><br>
+            <button>Save</button>
+        </form>
+    </div>
 <?php endif; ?>
 <script>
     var params = <?php echo json_encode($_GET)?>;
