dfaf7b55f2ce3a95216347d80f680aa81af3b94f
Equirizon <equirizon@gmail.com>
2024-01-30 01:56:21 +0800

    added `admin` styling

--- pages/admin/landing_page.php
+++ pages/admin/landing_page.php
@@ -1,14 +1,16 @@
 <head>
     <title>Welcome Admin</title>
 </head>
-<h1>
-    Admin Landing Page
-</h1>
-<a href="../pupclinic/php/ajax.php?action=logout">logout</a>
-<button onclick="redirectToRecords()">Records</button>
-<button onclick="redirectToUsers()">Users</button>
-<button onclick="redirectTo()">Real-Time Assessment</button>
 
+<div class="admin-container">
+    <h1>Admin Landing Page</h1>
+    <a href="../pupclinic/php/ajax.php?action=logout">logout</a>
+    <div class="admin-menu">
+        <button onclick="redirectToRecords()">Records</button>
+        <button onclick="redirectToUsers()">Users</button>
+        <button onclick="redirectTo()">Real-Time Assessment</button>
+    </div>
+</div>
 <script>
   function redirectToRecords() {
     window.location.href = "../pupclinic/admin.php?page=records&search=";
