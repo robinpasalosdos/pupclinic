8e7598a793d77a4cf74de96a72bcf603cd4ffe0c
Equirizon <equirizon@gmail.com>
2024-02-14 21:17:03 +0800

    styling

--- pages/menu.php
+++ pages/menu.php
@@ -1,5 +1,7 @@
+<div class="menu-tooltip">
 <h2>User Options</h2>
 <p>Please select one of the following options:</p>
+</div>
 <div class="menu_container">
     <button onclick="redirectToLoginMenu()">Login</button>
     <button onclick="redirectToRegister()">Register</button>
