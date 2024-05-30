08dc78b9b86bf1b38fabb03553fca7ada2216a83
robinhud07 <55908416+robinhud07@users.noreply.github.com>
2024-05-31 01:15:50 +0800

    cleaning

--- pages/dashboard/landing_page.php
+++ pages/dashboard/landing_page.php
@@ -7,29 +7,27 @@
   $query = "SELECT assessment_status FROM records WHERE user_id = $id AND assessment_status = 1;";
   $result = mysqli_query($conn, $query);
   
-  if(mysqli_num_rows($result) > 0)
-  {
-    ?>
+  if(mysqli_num_rows($result) > 0){ ?>
     <a class ="logout-button-a" href="../pupclinic/php/ajax.php?action=logout">logout</a>
     <div class="waiting_container">
-	<p>The physical examination is complete. Please proceed to the clinic staff for interview. You can now log-out.</p>
+	    <p>The physical examination is complete. Please proceed to the clinic staff for interview. You can now log-out.</p>
     </div>
-    <?php
-  }else{
-    ?>
-    <a class ="logout-button-a" href="../pupclinic/php/ajax.php?action=logout">logout</a>
-    <div class="main-menu">
-        <h2>Welcome to PupClinic Dashboard</h2>
-        <p>Manage your health and records efficiently.</p>
-        <div class="menu_container">
-        <button onclick="redirectToAssessment()">Physical Examination</button>
-        <button onclick="redirectToYourRecords()">Check Your Health Record</button>
-        <button onclick="redirectToYourProfile()">Your Profile</button>
-        </div>
-    </div>
-    <?php
-  }
-?>
+    <?php }else{ ?>
+      <a class ="logout-button-a" href="../pupclinic/php/ajax.php?action=logout">logout</a>
+      <div class="main-menu">
+          <h2>Welcome to PupClinic Dashboard</h2>
+          <p>Manage your health and records efficiently.</p>
+          <div class="menu_container">
+          <?php if($_SESSION['user'] == "guest"){?>
+            <button onclick="redirectToAssessment()">Physical Examination</button>
+          <?php }else{ ?>
+            <button onclick="redirectToAssessment()">Physical Examination</button>
+            <button onclick="redirectToYourRecords()">Check Your Health Record</button>
+            <button onclick="redirectToYourProfile()">Your Profile</button>
+          <?php } ?>
+          </div>
+      </div>
+    <?php } ?>
 <script>
   function redirectToYourRecords() {
     window.location.href = "../pupclinic/dashboard.php?page=patient_records&search=";
